a41d8d88c53affd2e12a9b74768e0dc5ca44e967
Ryan Asher <rasher@tribune.com>
2015-01-22 23:48:16 -0600

    Initial commits for 'Alone and Naked' and 'Data Breach' games, which are still work in progress.

---
+++ data_breach.py
@@ -0,0 +1,4 @@
+# This game is a WIP.
+# In this game, you are a hacker who is breaking into the American Intelligence Agency for some classified information.
+
+print "Your pudgy, white fingers sidle up to the keys on the keyboard."
