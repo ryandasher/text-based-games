7f9dc797205ac2a281ee8d8bec57ec56e5fe1dd0
Ryan Asher <ryandeanasher@gmail.com>
2016-01-31 18:16:10 -0500

    Data Breach: Print jargon to Terminal
    Update the output_jargon function so it prints a neverending stream of random characters. Add a basic
    command line prompt.
    
    Update the README.

--- data_breach_game/data_breach.py
+++ data_breach_game/data_breach.py
@@ -1,12 +1,27 @@
-import cmd, textwrap, sys, time
+import cmd, textwrap, random, string, sys, time
+
+from threading import Thread
 
 print "Your pudgy fingers sidle up to the keys on the keyboard."
 
 def output_jargon(looping = True):
+	characters = string.ascii_letters + string.digits + string.punctuation
 	while looping:
-		time.sleep(.1)
-		print '.',
+		time.sleep(.02)
+		character = random.choice(characters)
+		print character,
 		sys.stdout.flush()
 
 
+class DataBreachCmd(cmd.Cmd):
+	prompt = '\n> '
+
+	def default(self, arg):
+		print '%s is not a valid command. Type "help" for a list of commands.'
+
+
 output_jargon()
+# hacking_display = Thread(target = output_jargon)
+# hacking_display.daemon = True
+# hacking_display.start()
+# DataBreachCmd().cmdloop()
