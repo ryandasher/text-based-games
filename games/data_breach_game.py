2ed19c74eaddc90eac04c04866dfb6c2fca089e4
Ryan Asher <ryandeanasher@gmail.com>
2016-02-28 21:47:43 -0500

    Data Breach Updates:
    Finish a working draft of the Data Breach game. Include a list of words in the data
    directory.
    
    Update the README and the gitignore file.

--- games/data_breach_game.py
+++ games/data_breach_game.py
@@ -1,71 +1,82 @@
-import random, string, sys, textwrap, time
+import json, random, string, sys, textwrap, time
 
 import standard_commands
+
+SECONDS = 30
 
 print """
 Game Rules:
 When the game starts, your terminal window will begin to fill with characters.
-Hidden in the random characters are actual words that make up a code phrase.
-Find the code phrase, and enter it into the command prompt to hack into the
-mainframe. The command prompt will appear again after the program has finished
-running. You will have 20 seconds to enter the phrase, otherwise the alarm
-will sound and the FBI will be sent to your door. Type 'begin' to start the
-program.
-"""
+Hidden in the random characters are words that make up the password to access
+the mainframe. Find all of the words, and enter them into the command prompt
+when the output stops. Leave a single space between each word, do not enter any
+commas. You will have %s seconds to enter the words, otherwise the alarm will
+sound and the FBI will be sent to your door. Type 'begin' to start the program.
+""" % (SECONDS)
 
+# TODO: Add documentation comments.
 class DataBreachCmd(standard_commands.StandardCommands):
 	prompt = '\n> '
 
 
-	def __init__(self, words):
+	def __init__(self):
 		standard_commands.StandardCommands.__init__(self)
-		self.words = words
+		self.game = DataBreach()
+		self.passphrase = self.game.create_passphrase()
 
 
 	def default(self, arg):
-		print arg
-		if arg == self.words:
+		if arg == self.passphrase:
 			print "Congrats. You're in the mainframe!"
+			return True
 		else:
-			super(DataBreachCmd, self).default()
-
-
-	def do_greet(self, arg):
-		"""Exits the program."""
-		print "Hey!"
+			standard_commands.StandardCommands.default(self, arg)
 
 
 	def do_begin(self, arg):
 		"""Starts the program."""
-		DataBreach(self.words)
+		self.game.output_characters()
 
 
+# TODO: Add documentation comments.
 class DataBreach(object):
 
-	def __init__(self, phrase):
-		self.sleep_time = .08
-		self.phrase = phrase
-		self.start()
+	def __init__(self):
+		self.sleep_time = .03
+		self.word_list = self.load_word_list()
 
-	def start(self, looping = True):
+
+	def load_word_list(self):
+		with open('data/data_breach_word_list.json') as json_file:
+			words = json.load(json_file)
+		# Return a list of randomly selected words. Words can be repeated.
+		return [words[random.randint(0, 58)] for i in range(5)]
+
+
+	def create_passphrase(self):
+		return ' '.join(self.word_list)
+
+
+	def output_characters(self):
 		characters = string.ascii_letters + string.digits + string.punctuation
-		while looping:
+		while len(self.word_list):
 			time.sleep(self.sleep_time)
-			random_int = random.randrange(100)
-			if random_int >= 99:
-				self.show_word(self.phrase)
+			if random.randrange(1000) >= 995:
+				self.show_word(self.word_list[0])
+				self.word_list.pop(0)
 			else:
 				character = random.choice(characters)
 				print character,
 				sys.stdout.flush()
 
+
 	def show_word(self, word):
 		counter = 0
 		for letter in word:
-			time.sleep(self.sleep_time)
 			print word[counter],
 			sys.stdout.flush()
+			time.sleep(self.sleep_time)
 			counter += 1
 
 
-DataBreachCmd("Test").cmdloop()
+DataBreachCmd().cmdloop()
