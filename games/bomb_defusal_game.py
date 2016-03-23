769a0aed4790c5546a49f5f7917f383daf9826de
Ryan Asher <ryandeanasher@gmail.com>
2016-03-22 22:25:41 -0400

    Bomb Defusal: Refactoring
    Make the bomb defusal game much simpler, and remove the small narrative. This way it can be a
    portable minigame inside other games, in case I want to have a bomb defusal component. The
    game is still a work in progress at this point.

--- games/bomb_defusal_game.py
+++ games/bomb_defusal_game.py
@@ -1,39 +1,56 @@
 from threading import Thread
 from time import sleep
 
-import json, sys
+import sys
 
 import standard_commands
-
-STORY = open("data/bomb_defusal.json", "r")
-PARSED_JSON = json.loads(STORY)
 
 class BombDefusalCmd(standard_commands.StandardCommands):
     """Commands specific to the Bomb Defusal Game."""
     def __init__(self):
+        standard_commands.StandardCommands.__init__(self)
         self.game = BombDefusal()
+        self.game.start_countdown()
 
 
     def do_green(self, arg):
         """Cut a wire."""
         self.game.cut_wire(arg)
+        if any(color in self.game.cut_wires for colors in ('red', 'green')):
+            if self.game.counted_down:
+                print "The timer ran out. You've been incinerated."
+            return True
 
 
-    def do_answer(self, arg):
-        """Answer the phone."""
-        pass
+    def default(self, arg):
+        if self.game.counted_down:
+            print "The timer ran out. You've been incinerated."
+            return True
+        else:
+            standard_commands.StandardCommands.default(self, arg)
 
 
     do_red = do_green
     do_yellow = do_green
-    do_phone = do_answer
 
 
 class BombDefusal(object):
     """Bomb Defusal game class that controls all the game logic."""
     def __init__(self):
         self.counted_down = False
-        self.green_wire, self.yellow_wire, self.red_wire = "Intact"
+        self.wires = {
+            'green': 'The bomb has been deactivated.',
+            'yellow': 'Nothing happens.',
+            'red': 'The bomb explodes. You have been incinerated.'
+        }
+        self.cut_wires = []
+
+
+    def start_countdown(self):
+        """Call the methods associated with running game logic."""
+        countdown = Thread(target = self.countdown, args = (5,))
+        countdown.daemon = True
+        countdown.start()
 
 
     def countdown(self, seconds):
@@ -49,19 +66,6 @@ class BombDefusal(object):
         self.counted_down = True
 
 
-    def timer_beeps(self, amount):
-        """
-        Let's show some incremental beeps to increase the tension.
-
-        Args:
-        amount -- How many beeps we want to display (integer).
-        """
-        while amount > 0:
-            print "(Beep)...\n"
-            amount -= 1
-            sleep(1)
-
-
     def cut_wire(self, color):
         """
         Cut one of the wires.
@@ -69,7 +73,8 @@ class BombDefusal(object):
         Args:
         color -- The color of the wire you intend to cut (string).
         """
-        pass
+        print self.wires[color]
+        self.cut_wires.append(color)
 
 
 def print_characters(characters):
@@ -83,113 +88,13 @@ def print_characters(characters):
     for character in characters:
         print character,
         sys.stdout.flush()
-        time.sleep(.05)
+        sleep(.2)
 
 
-def choices(color):
-    """
-    This function contains all the conditional logic when making choices.
-    I know this is one giant gross conditional. I will think of better ways
-    to do this in the future.
-    """
-    if color.lower() == 'green':
-        line_number = 8
-        while line_number < 12:
-            print story_lines[line_number]
-            line_number += 1
-        # TODO: We need a way to stop the initial countdown
-        short_countdown = Thread(target = total_countdown, args = (60,))
-        short_countdown.daemon = True
-        short_countdown.start()
-        second_choice = raw_input("Choose a colored wire to cut, or do something else...\n>")
-        print "\n"
-        if second_choice.lower() == 'yellow':
-            print story_lines[17]
-            phone_choice = raw_input("Do you answer the phone?\n>")
-            print "\n"
-            if 'y' in phone_choice.lower():
-                print story_lines[25]
-                close_it_up()
-            else:
-                print story_lines[23]
-                close_it_up()
-        elif second_choice.lower() == 'red':
-            story_lines[19]
-            close_it_up()
-        else:
-            print "As you attempt to %s, your phone rings." % second_choice
-            phone_choice = raw_input("Do you answer the phone?\n>")
-            print "\n"
-            if 'y' in phone_choice.lower():
-                print story_lines[25]
-                close_it_up()
-            else:
-                print story_lines[23]
-                close_it_up()
-    elif color.lower() == 'yellow':
-        print story_lines[13]
-        second_choice = raw_input("Choose another wire to cut...\n>")
-        print "\n"
-        if second_choice.lower() == 'green':
-            line_number = 8
-            while line_number < 12:
-                print story_lines[line_number]
-                line_number += 1
-            # TODO: We need a way to stop the initial countdown
-            short_countdown = Thread(target = total_countdown, args = (60,))
-            short_countdown.daemon = True
-            short_countdown.start()
-            tertiary_choice = raw_input("Cut the red wire, or do something else...\n>")
-            print "\n"
-            if tertiary_choice.lower() == 'red':
-                print story_lines[19]
-                close_it_up()
-            else:
-                print "As you attempt to %s, your phone rings." % second_choice
-                phone_choice = raw_input("Do you answer the phone?\n>")
-                print "\n"
-                if 'y' in phone_choice.lower():
-                    print story_lines[25]
-                    close_it_up()
-                else:
-                    print story_lines[23]
-                    close_it_up()
-        elif second_choice.lower() == 'red':
-            print story_lines[19]
-            close_it_up()
-        else:
-            print story_lines[21]
-            color_choice = raw_input("What color wire will you cut?\n>")
-            print "\n"
-            if color_choice.lower() == 'yellow':
-                print "Your incompetence causes your partner to push you aside and handle the task without your help."
-                close_it_up()
-            else:
-                choices(color_choice)
-    elif color.lower() == 'red':
-        print story_lines[15]
-        close_it_up()
-    else:
-        print story_lines[21]
-        color_choice = raw_input("What color wire will you cut?\n> ")
-        print "\n"
-        choices(color_choice)
+introduction = [
+    "There is a bomb sitting in front of you...\n",
+    "It will detonate in one minute."
+]
 
-
-print story_lines[0]
-sleep(3)
-timer_beeps(3)
-n = 1
-while n < 6:
-    print story_lines[n]
-    n += 1
-    enter = raw_input("(continue)\n")
-
-countdown = Thread(target = total_countdown, args = (277,))
-# Set the thread to a daemon thread, so it dies when we exit the file.
-countdown.daemon = True
-countdown.start()
-
-color_choice = raw_input("What color wire will you cut?\n> ")
-print "\n"
-choices(color_choice)
+print_characters(introduction)
+BombDefusalCmd().cmdloop()
